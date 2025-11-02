a4cdf0cdd39967f3fd6e7c49cffe21cf984a1d6e
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2025-11-02 15:22:49 +0900

    first commit

---
+++ src/main/java/com/glow/Glaw/GlawApplication.java
@@ -0,0 +1,13 @@
+package com.glow.Glaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glow/Glaw/GlawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glow.Glaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
