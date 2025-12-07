3e9c2f7855ac7a3d104cf2270f3f3f6d5f7b06b3
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2025-12-08 06:40:10 +0900

    feat/#1-CommonException

--- src/main/java/com/glow/Glaw/global/error/exception/CommonException.java
+++ src/main/java/com/glow/Glaw/global/error/exception/CommonException.java
@@ -1,7 +1,16 @@
 package com.glow.Glaw.global.error.exception;
 
+import com.glow.Glaw.global.error.ErrorCode;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
 public class CommonException extends RuntimeException {
-	public CommonException(String message) {
-		super(message);
+	private final ErrorCode errorCode;
+
+	public CommonException(ErrorCode errorCode) {
+		super(errorCode.getMessage()); // 👈 메시지 전달 추가! 토큰 에러응답을 위해서
+		this.errorCode = errorCode;
 	}
 }
