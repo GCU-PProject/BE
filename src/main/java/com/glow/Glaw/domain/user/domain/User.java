323a7ce279d8ee6bddbf5733a3bc699dccf3031f
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2026-01-27 14:21:30 -0800

    feat/#1-domain

---
+++ src/main/java/com/glow/Glaw/domain/user/domain/User.java
@@ -0,0 +1,45 @@
+package com.glow.Glaw.domain.user.domain;
+
+import com.glow.Glaw.domain.shared.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false)
+	private String name;
+
+	// Builder를 통해서만 객체 생성
+	@Builder
+	private User(String email, String name) {
+		this.email = email;
+		this.name = name;
+	}
+
+	// 사용자 생성 Method
+	public static User createUser(String email, String name) {
+		return User.builder()
+			.email(email)
+			.name(name)
+			.build();
+	}
+}
---
+++ src/main/java/com/glow/Glaw/global/auth/login/handler/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,5 @@
+package com.glow.Glaw.global.auth.login.handler;
+
+public class OAuth2LoginSuccessHandler {
+
+}
