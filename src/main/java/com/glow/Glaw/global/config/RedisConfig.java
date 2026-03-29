8bd50b8b379356369d390a433ae2c5243a6e16e6
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2026-03-30 03:16:39 +0900

    fix/#12-RedisConfig 파일 수정

--- src/main/java/com/glow/Glaw/global/config/RedisConfig.java
+++ src/main/java/com/glow/Glaw/global/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.glow.Glaw.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -8,10 +9,16 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
+	@Value("${spring.data.redis.host}")
+	private String redisHost;
+
+	@Value("${spring.data.redis.port}")
+	private int redisPort;
+
 	// Redis 연결 설정
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory("localhost", 6379);
+		return new LettuceConnectionFactory(redisHost, redisPort);
 	}
 
 	// StringRedisTemplate 생성
