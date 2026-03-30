5bedcf74e5f2d47dada6347fe4d6fa0a26783af3
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2026-03-31 05:57:04 +0900

    feat/#9-law controller update

--- src/main/java/com/glow/Glaw/domain/law/controller/LawController.java
+++ src/main/java/com/glow/Glaw/domain/law/controller/LawController.java
@@ -2,12 +2,16 @@ package com.glow.Glaw.domain.law.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.glow.Glaw.domain.law.dto.LawDetailResponseDto;
 import com.glow.Glaw.domain.law.dto.LawListResponseDto;
 import com.glow.Glaw.domain.law.service.LawService;
+import com.glow.Glaw.global.response.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,7 +23,23 @@ public class LawController {
 
 	// 법률 리스트 조회
 	@GetMapping
-	public List<LawListResponseDto> getLawList() {
-		return lawService.getLawList();
+	public ResponseEntity<ApiResponse<List<LawListResponseDto>>> getLawList() {
+		List<LawListResponseDto> response = lawService.getLawList();
+
+		return ResponseEntity.ok(
+			ApiResponse.success("법률 리스트 조회 성공", response)
+		);
+	}
+
+	// 법률 상세 조회
+	@GetMapping("/{lawId}")
+	public ResponseEntity<ApiResponse<LawDetailResponseDto>> getLawDetail(
+		@PathVariable Long lawId
+	){
+		LawDetailResponseDto response = lawService.getLawDetail(lawId);
+
+		return ResponseEntity.ok(
+			ApiResponse.success("법률 상세 조회 성공", response)
+		);
 	}
 }
--- src/main/java/com/glow/Glaw/domain/law/dto/LawListResponseDto.java
+++ src/main/java/com/glow/Glaw/domain/law/dto/LawListResponseDto.java
@@ -3,7 +3,6 @@ package com.glow.Glaw.domain.law.dto;
 import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
